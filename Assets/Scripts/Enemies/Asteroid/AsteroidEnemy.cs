9a6085a05e9e541921dfd500a585b667ab741493
Cmanchik <aksenova1648@gmail.com>
2025-07-30 11:47:04 +0400

    Логика врага Астероид

---
+++ Assets/Scripts/Enemies/Asteroid/AsteroidEnemy.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Enemies.Asteroid
+{
+    public class AsteroidEnemy : EnemyAbstract
+    {
+        public override void OnCollisionEnter(Collision collision)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/Enemies/EnemyAbstract.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts.Enemies
+{
+    public abstract class EnemyAbstract : MonoBehaviour
+    {
+        [SerializeField]
+        private int hp;
+
+
+        public abstract void OnCollisionEnter(Collision collision);
+    }
+}
