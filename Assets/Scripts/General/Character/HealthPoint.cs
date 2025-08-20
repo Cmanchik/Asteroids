6804315848e384b9612c61bd5c419e95f35ebd2f
Cmanchik <aksenova1648@gmail.com>
2025-08-20 21:35:02 +0400

    Создание компонента здоровья

---
+++ Assets/Scripts/General/Character/HealthPoint.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Assets.Scripts.General.Character
+{
+    public class HealthPoint : MonoBehaviour
+    {
+        [SerializeField]
+        private int health;
+
+        public int Health { get => health; set => health = value; }
+
+        public UnityEvent OnDeath;
+
+        public UnityEvent OnTakingDamage;
+
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+
+            if (health <= 0) OnDeath?.Invoke();
+            else OnTakingDamage?.Invoke();
+        }
+    }
+}
