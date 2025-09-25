4d64900fc2b70f73562c2b583a42c5f8bab8e5e5
Cmanchik <aksenova1648@gmail.com>
2025-09-25 15:21:15 +0400

    Меню паузы

---
+++ Assets/Scripts/UI/GamePlayUI/PausePanel/PausePanel.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace Assets.Scripts.UI.GamePlayUI.PausePanel
+{
+    public class PausePanel : MonoBehaviour
+    {
+        [SerializeField]
+        private Button m_continueButton;
+
+        [SerializeField]
+        private Button m_mainMenuButton;
+
+        private void Awake()
+        {
+            m_continueButton.onClick.AddListener(() => { gameObject.SetActive(false); });
+            m_mainMenuButton.onClick.AddListener(() => { SceneManager.LoadScene("MainMenuScene"); });
+        }
+
+        private void OnEnable()
+        {
+            Time.timeScale = 0;
+        }
+
+        private void OnDisable()
+        {
+            Time.timeScale = 1;
+        }
+    }
+}
---
+++ Assets/Scripts/UI/GamePlayUI/PausePanel/PausePanelContoller.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace Assets.Scripts.UI.GamePlayUI.PausePanel
+{
+    public class PausePanelContoller : MonoBehaviour
+    {
+        [SerializeField]
+        private GameObject m_pausePanel;
+
+        private void Awake()
+        {
+            InputSystem.actions.FindAction("Cancel").started += ChangeStatePanel;
+        }
+
+        private void ChangeStatePanel(InputAction.CallbackContext context)
+        {
+            if (m_pausePanel == null) return;
+
+
+            if (m_pausePanel.activeSelf == false)
+            {
+                m_pausePanel.SetActive(true);
+            }
+            else
+            {
+                m_pausePanel.SetActive(false);
+            }
+
+            Debug.Log("Sosal?");
+        }
+    }
+}
