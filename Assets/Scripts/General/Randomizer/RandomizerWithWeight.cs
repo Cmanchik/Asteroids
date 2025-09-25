a30d6cdfe5fc556566844f469edd8fbb9685cb7b
Cmanchik <aksenova1648@gmail.com>
2025-09-25 17:22:28 +0400

    Рандомайзер с учетом весов объектов

---
+++ Assets/Scripts/General/Randomizer/RandomizerWithWeight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Linq;
+using UnityEngine;
+
+namespace Assets.Scripts.General.Randomizer
+{
+    public class RandomizerWithWeight
+    {
+        public static RandomObject GetRandomObject(RandomObject[] objects)
+        {
+            int totalWeight = objects.Sum(x => x.Weight);
+            int randomNumber = Random.Range(0, totalWeight);
+
+            return objects.OrderBy(x => Mathf.Abs(x.Weight - randomNumber)).First();
+        }
+    }
+
+    public struct RandomObject
+    {
+        public GameObject Object;
+        public int Weight;
+    }
+}
